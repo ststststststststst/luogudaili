834df6c91f6efbe954eaf9c0b389a6b9d468bbdf
sunny_town的博客技术支持 <13997766336@163.com>
2025-07-20 16:15:27 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,16 +1,23 @@
- https = require('https');
+
+const https = require('https');
 module.exports = (req, res) => {
-  const path = req.url.replace(/^\/api\//, '');
+  const path = req.query.path || '';
   https.get({
     hostname: 'www.luogu.com',
     path: '/' + path,
     headers: {
-      ...req.headers,
-      host: 'www.luogu.com',
-      'x-real-ip': '203.198.7.16'
+      'host': 'www.luogu.com',
+      'x-real-ip': '203.198.7.16',
+      'accept-language': 'en-US'
     }
   }, (proxyRes) => {
-    res.writeHead(proxyRes.statusCode, proxyRes.headers);
+    res.writeHead(proxyRes.statusCode, {
+      ...proxyRes.headers,
+      'x-vercel-region': 'hkg1'
+    });
     proxyRes.pipe(res);
-  }).on('error', () => res.status(502).end());
+  }).on('error', (e) => {
+    console.error(e);
+    res.status(502).send('Proxy Error');
+  });
 };
