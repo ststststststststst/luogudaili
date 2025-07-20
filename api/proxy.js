94de94f230be66ff0970adb2d9ec4f7cdb85881d
sunny_town的博客技术支持 <13997766336@163.com>
2025-07-20 15:06:17 +0800

    Update proxy.js

--- api/proxy.js
+++ api/proxy.js
@@ -1,39 +1,11 @@
-import fetch from 'node-fetch';
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-export default async (req, res) => {
-  try {
-    const targetUrl = `https://www.luogu.com${req.url}`; 
-
-    if (req.url.includes("/lg4/captcha")) {
-      const captchaResp = await fetch(targetUrl, {
-        headers: {
-          "Host": "www.luogu.com",
-          "Cookie": req.headers.cookie || "",
-          "User-Agent": "Mozilla/5.0"
-        }
-      });
-      res.setHeader("Content-Type", captchaResp.headers.get("content-type"));
-      return captchaResp.body.pipe(res);
+module.exports = (req, res) => {
+  createProxyMiddleware({
+    target: 'https://www.luogu.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/proxy': ''
     }
-
-    const response = await fetch(targetUrl, {
-      headers: {
-        ...req.headers,
-        "Host": "www.luogu.com"  
-      }
-    });
-    if (response.headers.get("content-type")?.includes("text/html")) {
-      let html = await response.text();
-      html = html.replace(
-        /(src|href)=(["'])(\/[^"']+)/g,
-        `$1=$2https://${req.headers.host}$3`
-      );
-      res.send(html);
-    } else {
-      response.body.pipe(res);
-    }
-  } catch (error) {
-    console.error("Proxy Error:", error);
-    res.status(500).json({ error: "Internal Server Error" });
-  }
+  })(req, res);
 };
